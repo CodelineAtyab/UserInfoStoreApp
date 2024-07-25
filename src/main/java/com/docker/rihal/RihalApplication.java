59d8600867d5b6a364578159ef61b9b647a033b8
Ethar Al Aasmi <ethar.aasmi@rihal.om>
2024-07-25 11:54:31 +0400

    Added dockerfile and docker-compose

--- src/main/java/com/docker/rihal/RihalApplication.java
+++ src/main/java/com/docker/rihal/RihalApplication.java
@@ -1,13 +1,21 @@
 package com.docker.rihal;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RihalApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RihalApplication.class, args);
-	}
+    public static void main(String[] args) {
+        // Load environment variables from .env file
+        Dotenv dotenv = Dotenv.load();
+        System.setProperty("SPRING_DATASOURCE_URL", dotenv.get("SPRING_DATASOURCE_URL"));
+        System.setProperty("SPRING_DATASOURCE_USERNAME", dotenv.get("SPRING_DATASOURCE_USERNAME"));
+        System.setProperty("SPRING_DATASOURCE_PASSWORD", dotenv.get("SPRING_DATASOURCE_PASSWORD"));
+        System.setProperty("SPRING_JPA_HIBERNATE_DDL_AUTO", dotenv.get("SPRING_JPA_HIBERNATE_DDL_AUTO"));
+        System.setProperty("SPRING_JPA_SHOW_SQL", dotenv.get("SPRING_JPA_SHOW_SQL"));
 
+        SpringApplication.run(RihalApplication.class, args);
+    }
 }
